3336b327b23dd60ca38843acd8af1151048f069a
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2024-08-21 11:00:36 -0500

    Ajuste css

--- src/components/CardContent.jsx
+++ src/components/CardContent.jsx
@@ -5,9 +5,9 @@ import {product} from '../helpers/Api';
 
 export const CardContent = ({onProductClick} ) => {
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4 ">
+    <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
       {product.map((product) => (
-        <Card style={{ width: '18rem', margin: '60px', backgroundColor: '#e1e6e9' }} key={product.id}>
+      <Card style={{ width: '18rem', margin: '60px', backgroundColor: '#e1e6e9' }} key={product.id}>
         <Card.Img style={{marginTop: '10px' }} variant="top" src={product.img} />
         <Card.Body>
           <Card.Title>{product.title}</Card.Title>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,4 +13,4 @@ export const Footer = () => {
       </div>
     </Navbar>
   );
-};
+}
